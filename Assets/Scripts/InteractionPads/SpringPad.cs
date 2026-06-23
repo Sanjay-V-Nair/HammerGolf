1cc4357f8273af888f0082b5d0585a99d0fd14b0
Shlok <100297780+CriTMx@users.noreply.github.com>
2026-06-24 00:48:12 +0530

    remove rb reference from springpad

--- Assets/Scripts/InteractionPads/SpringPad.cs
+++ Assets/Scripts/InteractionPads/SpringPad.cs
@@ -5,11 +5,10 @@ namespace HammerGolf
     public class SpringPad : BaseInteractionPad
     {
         [SerializeField] float springForce = 30f;
-        [SerializeField] Rigidbody rb;
 
         protected override void ApplyEffect(Rigidbody ball, Collider other)
         {
-            var normal = rb.transform.up;
+            var normal = transform.up;
             ball.AddForce(normal * springForce, ForceMode.Impulse);
         }
     }
