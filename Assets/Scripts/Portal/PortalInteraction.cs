cc6649b2c33f75345ba926c0126dab25ba33e01d
Shlok <100297780+CriTMx@users.noreply.github.com>
2026-06-26 01:35:53 +0530

    portals!

---
+++ Assets/Scripts/Portal/PortalInteraction.cs
@@ -0,0 +1,54 @@
+using UnityEngine;
+
+namespace HammerGolf
+{
+    public class PortalInteraction : MonoBehaviour
+    {
+        [Header("Portal")]
+        [SerializeField] PortalInteraction linkedPortal;
+
+        [Header("Settings")]
+        [SerializeField] float minimumExitSpeed = 12f;
+        [SerializeField] float cooldown = 0.25f;
+
+        private float _lastTeleportTime;
+
+        public float Cooldown { get => cooldown; }
+        public float LastTeleportTime { get => _lastTeleportTime; }
+
+        private void OnTriggerEnter(Collider other)
+        {
+            if (!other.TryGetComponent(out Rigidbody rb))
+                return;
+
+            if (linkedPortal == null)
+                return;
+
+            if (Time.time - _lastTeleportTime < cooldown || Time.time - linkedPortal.LastTeleportTime < linkedPortal.Cooldown)
+                return;
+
+            Teleport(rb);
+        }
+
+        private void Teleport(Rigidbody ball)
+        {
+            linkedPortal._lastTeleportTime = Time.time;
+            _lastTeleportTime = Time.time;
+
+            Vector3 velocity = ball.linearVelocity;
+
+            float speed = Mathf.Max(
+                velocity.magnitude,
+                minimumExitSpeed);
+
+            Vector3 exitDirection = linkedPortal.transform.forward;
+
+            ball.position =
+                linkedPortal.transform.position +
+                exitDirection * 1.2f;
+
+            ball.linearVelocity =
+                exitDirection * speed;
+        }
+    }
+}
